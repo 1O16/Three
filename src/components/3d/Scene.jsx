d81290ee546b4da36bca8d977b37050707aa91f6
1O16 <youiiyou1253@gmail.com>
2023-04-25 15:07:20 +0900

    feat: Tree gredientMap 추가

--- src/components/3d/Scene.jsx
+++ src/components/3d/Scene.jsx
@@ -2,28 +2,6 @@ import React, { useRef, useState } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { Trees } from './Trees';
 
-function Box(props) {
-  const mesh = useRef();
-  const [hovered, setHovered] = useState(false);
-  const [active, setActive] = useState(false);
-  useFrame((state, delta) => (mesh.current.rotation.x += delta));
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
-      castShadow
-      onClick={(e) => setActive(!active)}
-      onPointerOver={(e) => setHovered(true)}
-      onPointerOut={(e) => setHovered(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'lime'} />
-    </mesh>
-  );
-}
-
 export const Scene = () => {
   return (
     <>
