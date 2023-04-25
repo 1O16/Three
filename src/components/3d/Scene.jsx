b7003bb08f03b035d10cb51d31c2d62c1fb58ca9
[1O16] <youiiyou1253@gmail.com>
2023-04-26 01:16:39 +0900

    feat: Tree 완성

--- src/components/3d/Scene.jsx
+++ src/components/3d/Scene.jsx
@@ -1,11 +1,21 @@
-import React, { useRef, useState } from 'react';
+import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { Trees } from './Trees';
+import { Color } from 'three';
 
-export const Scene = () => {
+export function Scene() {
+  const refTrees = useRef(null);
+
+  useFrame(() => {
+    const { current: group } = refTrees;
+    if (group) {
+      group.rotation.x = group.rotation.y += 0.01;
+    }
+  });
+
   return (
     <>
-      <ambientLight insentive={0.1} />
+      <ambientLight intensity={0.1} />
       <directionalLight
         color="white"
         position={[15, 15, 15]}
@@ -13,7 +23,25 @@ export const Scene = () => {
         shadow-mapSize-width={2048}
         shadow-mapSize-height={2048}
       />
-      <Trees />
+
+      <Trees
+        position={[0, 0, 0]}
+        colors={[
+          new Color('#427062').convertLinearToSRGB(),
+          new Color('#33594e').convertLinearToSRGB(),
+          new Color('#234549').convertLinearToSRGB(),
+          new Color('#1e363f').convertLinearToSRGB(),
+        ]}
+      />
+      <Trees
+        position={[0, 0, 4]}
+        colors={[
+          new Color('#4a8d7e').convertLinearToSRGB(),
+          new Color('#377f6a').convertLinearToSRGB(),
+          new Color('#184f52').convertLinearToSRGB(),
+          new Color('#143b36').convertLinearToSRGB(),
+        ]}
+      />
     </>
   );
-};
+}
